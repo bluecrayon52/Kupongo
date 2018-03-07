397c6fec3fbbb32e11ba7458893ca1260e82aca6
Preston <paturne4@uncg.edu>
2018-03-07 15:27:07 -0500

    Added sales excutive front-end code

---
+++ kupongo/imports/pages/RegisterPage.js
@@ -0,0 +1,31 @@
+/*
+Register page for new users
+*/
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import '../css/Register.css';
+import Register from '../components/Register'
+
+class RegisterPage extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div><Register/></div>
+        );
+    }
+}
+
+export default withTracker(() => {
+    //MongoDB subscription to userList
+    Meteor.subscribe('userList');
+    return {
+
+    };
+})(RegisterPage);
