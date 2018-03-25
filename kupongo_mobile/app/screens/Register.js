d493a2675eeb9e7ddbb418a20d8f32aeaaf69125
David Santana <santana21796@me.com>
2018-03-24 20:22:11 -0400

    Fixed registration bug, where `this` could not be used.

--- kupongo_mobile/app/screens/Register.js
+++ kupongo_mobile/app/screens/Register.js
@@ -1,9 +1,8 @@
 /**
- * Regsiter users.
+ * Register users.
  */
 
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import Meteor from 'react-native-meteor';
 
 import {
@@ -28,6 +27,7 @@ export default class Register extends Component {
       address: ''
     }
   }
+
   render() {
     return (
         <View style={styles.container}>
@@ -38,68 +38,70 @@ export default class Register extends Component {
             Register
           </Text>
           <TextInput
-           	 style={styles.input}
-        	 placeholder="Email"
-           	 ref="emailInput"
-             name="email"
-             onChangeText={(text) => this.setState({email: text})}
-          	/>
-        	<TextInput
-           	 style={styles.input}
-        	 placeholder="First Name"
-           	 ref="firstNameInput"
-             name="firstName"
-             onChangeText={(text) => this.setState({firstName: text})}
-          	/>
-        	<TextInput
-           	 style={styles.input}
-        	 placeholder="Last Name"
-           	 ref="lastNameInput"
-             name="lastName"
-             onChangeText={(text) => this.setState({lastName: text})}
-          	/>
-        	<TextInput
-           	 style={styles.input}
-        	 placeholder="Phone Number"
-           	 ref="phoneNumberInput"
-             name="phoneNumber"
-             onChangeText={(text) => this.setState({phoneNumber: text})}
-          	/>
-        	<TextInput
               style={styles.input}
-        		  placeholder="Password"
+              placeholder="Email"
+              ref="emailInput"
+              name="email"
+              onChangeText={(text) => this.setState({email: text})}
+          />
+          <TextInput
+              style={styles.input}
+              placeholder="First Name"
+              ref="firstNameInput"
+              name="firstName"
+              onChangeText={(text) => this.setState({firstName: text})}
+          />
+          <TextInput
+              style={styles.input}
+              placeholder="Last Name"
+              ref="lastNameInput"
+              name="lastName"
+              onChangeText={(text) => this.setState({lastName: text})}
+          />
+          <TextInput
+              style={styles.input}
+              placeholder="Phone Number"
+              ref="phoneNumberInput"
+              name="phoneNumber"
+              onChangeText={(text) => this.setState({phoneNumber: text})}
+          />
+          <TextInput
+              style={styles.input}
+              placeholder="Password"
               ref="passwordinput"
               name="password"
               onChangeText={(text) => this.setState({password: text})}
               secureTextEntry={true}
-                       />
-                       <TextInput
-                        	 style={styles.input}
-                     	 placeholder="Address"
-                        	 ref="addressInput"
-                           name="address"
-                          onChangeText={(text) => this.setState({address: text})}
-                       	/>
-                        <Button
-                            onPress={() => {this.props.navigation.navigate(this.process(this), {
-                              }
-                            )}}
-                            title="Submit"
-                        />
+          />
+          <TextInput
+              style={styles.input}
+              placeholder="Address"
+              ref="addressInput"
+              name="address"
+              onChangeText={(text) => this.setState({address: text})}
+          />
+          <Button
+              onPress={() => {
+                this.process();
+              }}
+              title="Submit"
+          />
         </View>
     );
   }
-  process(ok) {
- //TODO(David) figure out why cant use this in here... get text input to const to be sent Database
-    {this.props.navigation.navigate('Home', {
-      user: {
-        _id: 'asdf',
-        couponList: new Set()
-      }}
-    )}
+
+  process() {
+    this.props.navigation.navigate('Home', {
+          user: {
+            _id: 'asdf',
+            couponList: new Set()
+          }
+        }
+    );
   }
+
   componentWillMount() {
-   // Make sure you run "npm run start" on the kupongo project so the server is up.
+    // Make sure you run "npm run start" on the kupongo project so the server is up.
     // This connects to that Meteor server. Once the AWS server is up, replace the ip address with the url of the server.
     // If you are on Android, find your network IP address, the one which your WiFi is using.
     // If you are on iOS, use localhost instead of your IP address.
@@ -123,7 +125,7 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   input: {
-     paddingHorizontal: 10,
+    paddingHorizontal: 10,
   },
   instructions: {
     textAlign: 'center',
